94c88c9d2b22a9abe2e1840650556b763f0d781f
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2024-05-26 00:49:24 +0300

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./about.css"
+import "./about.css";
 import img_1 from "../../assets/pls-011.jpg.png";
 import img_2 from "../../assets/9c61052a0d746431fd27de5185f410fc.png";
 import img_3 from "../../assets/a6c7eb7bd18d00c4508f82ffaa65a7cf.jpeg";
@@ -10,12 +10,10 @@ import img_card_2 from "../../assets/pls-05.jpg.png";
 import img_card_3 from "../../assets/pls-07.jpg.png";
 import img_card_4 from "../../assets/pls-06.jpg.png";
 
-
 function About() {
   return (
     <div className="about" id="About">
       <div className="container">
-
         <div className="aboutt">
           <div className="about_left">
             <div className="img_1">
@@ -23,17 +21,17 @@ function About() {
             </div>
             <div className="img_2">
               <img className="im_2" src={img_2} alt="" />
-          </div>
+            </div>
           </div>
           <div className="about_right">
             <button>ABOUT US</button>
             <h2>APEX 180</h2>
             <p>
-              To achieve excellence in dentistry through up-to-date technology and
-              highly educated professionals.To change lives everyday by creating
-              healthy and beautiful smiles.
+              To achieve excellence in dentistry through up-to-date technology
+              and highly educated professionals.To change lives everyday by
+              creating healthy and beautiful smiles.
             </p>
-          
+
             <div className="provide">
               <img src={img_3} alt="" />
               <p>
@@ -41,47 +39,50 @@ function About() {
                 honesty,compassion, quality and integrity.
               </p>
             </div>
-          
+
             <div className="ahmed">
+              <div>
+                <img className="circle" src={img_4} alt="" />
                 <div>
-                   <img className="circle" src={img_4} alt="" />
-                   <div>
-                      <h4>DR.Ahmed Moftah</h4>
-                      <p>MAIN DOCTOR</p>
-                   </div>
+                  <h4>DR.Ahmed Moftah</h4>
+                  <p>MAIN DOCTOR</p>
                 </div>
-                <img className="rrrr" src={img_5} alt="" />
+              </div>
+              <img className="rrrr" src={img_5} alt="" />
             </div>
-          
-            <p>Bachelor's degree in Oral and Dental Medicine
-           a British Fellowship in Cosmetic and Implant Dentistry from the Royal College of Physicians and Surgeons - Glasgow and the Royal College of Surgeons - England - United Kingdom)
-          f the American Society for Cosmetic and Implant Dentistry</p>
+
+            <p>
+              Bachelor's degree in Oral and Dental Medicine a British Fellowship
+              in Cosmetic and Implant Dentistry from the Royal College of
+              Physicians and Surgeons - Glasgow and the Royal College of
+              Surgeons - England - United Kingdom) f the American Society for
+              Cosmetic and Implant Dentistry
+            </p>
           </div>
         </div>
 
         <div className="parent_card">
-            <div className="card">
-                 <img src={img_card_1} alt="" />
-                 <h4>Wisdom Teeth Removal</h4>
-                 <p>FROM 1299 $</p>
-            </div>
-            <div className="card">
-                 <img src={img_card_2} alt="" />
-                 <h4>Dental Implant Surgery</h4>
-                 <p>FROM 350 $</p>
-            </div>
-            <div className="card">
-                 <img src={img_card_3} alt="" />
-                 <h4>Bone Grafing Procedures</h4>
-                 <p>FROM 2000 $</p>
-            </div>
-            <div className="card">
-                 <img src={img_card_4} alt="" />
-                 <h4>Oral Cancer Screening</h4>
-                 <p>FROM 150 $</p>
-            </div>
+          <div className="card">
+            <img src={img_card_1} alt="" />
+            <h4>Wisdom Teeth Removal</h4>
+            <p>FROM 1299 $</p>
+          </div>
+          <div className="card">
+            <img src={img_card_2} alt="" />
+            <h4>Dental Implant Surgery</h4>
+            <p>FROM 350 $</p>
+          </div>
+          <div className="card">
+            <img src={img_card_3} alt="" />
+            <h4>Bone Grafing Procedures</h4>
+            <p>FROM 2000 $</p>
+          </div>
+          <div className="card">
+            <img src={img_card_4} alt="" />
+            <h4>Oral Cancer Screening</h4>
+            <p>FROM 150 $</p>
+          </div>
         </div>
-
       </div>
     </div>
   );
